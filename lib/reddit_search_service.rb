304a52a30a35aa857b561e72210e087fa3830fd8
benjaminharel <benjamin@skanax.com>
2013-03-21 18:08:57 +0200

    fixed complex queries

--- lib/reddit_search_service.rb
+++ lib/reddit_search_service.rb
@@ -1,6 +1,8 @@
 class RedditSearchService
+
   def self.search (query)
-    uri = URI.parse("http://www.reddit.com/search.json?q=" + query)
+    args = { :q => query }.to_query
+    uri = URI.parse("http://www.reddit.com/search.json?" + args)
     response = Net::HTTP.get_response(uri)
     JSON.parse response.body
   end
